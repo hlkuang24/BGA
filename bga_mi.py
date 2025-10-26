98598b50d5288e41a2beff3fe81aa2f5954fd48d
liu yuan <62823999+hlkuang24@users.noreply.github.com>
2025-10-26 21:16:22 +0800

    Update bga_mi.py

--- bga_mi.py
+++ bga_mi.py
@@ -1,6 +1,3 @@
-# coding=utf-8
-"""Implementation of MI-FGSM attack."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -292,3 +289,4 @@ def load_labels(file_name):
 
 if __name__ == '__main__':
     tf.app.run()
+
